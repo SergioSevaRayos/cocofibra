daec13207d623dc59e9852b05b6e4314b233b9be
SergioSevaRayos <sergiosevarayos@gmail.com>
2026-02-23 16:59:50 +0100

    Versión 1.0 Pendiente de validación

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  // 👇 AÑADE ESTA LÍNEA CON EL DOMINIO REAL (no olvides la coma al final)
+  site: 'https://www.cocofibramovil.es', 
+  integrations: [tailwind(), sitemap()],
 });
